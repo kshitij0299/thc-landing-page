63826a6e1cd75a1f58fe13fb1cbf214afa931ae5
kshitij <kshitij0299@gmail.com>
2025-12-31 00:59:58 +0530

    Remove basePath and assetPrefix from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/thc-landing-page",
-  assetPrefix: "/thc-landing-page",
   images: {
     unoptimized: true,
   },
